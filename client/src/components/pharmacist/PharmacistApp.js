9ff1fd39d0a54151499e4e31d55e6277b9c0554b
Kyler Sakumoto <kylerws@uw.edu>
2021-04-16 17:07:17 -0700

    Added pharmacist routes

--- client/src/components/pharmacist/PharmacistApp.js
+++ client/src/components/pharmacist/PharmacistApp.js
@@ -1,37 +1,87 @@
 import React from 'react'
 import { useAuth } from '../../hooks/useAuth'
+import { Route, Switch, Redirect } from 'react-router-dom'
 import { Container, Jumbotron, Row, Col, Button } from 'react-bootstrap'
 
 const PharmacistApp = () => {
-  
 
   return (
-   <Dashboard /> 
+    <Container fluid className="bg-light vh-100">
+      <Row className="h-100">
+        <Col xs={2} md={1} lg={4} xl={3} className="d-flex flex-column">
+          <SideBar />
+        </Col>
+        <Col id="dashboardContent">
+          <Switch>
+            <Route path="/pharmacist/dashboard" component={Dashboard}/>
+            <Route path="/pharmacist/inventory" component={Inventory}/>
+            <Route path="/pharmacist/orders" component={Orders} />
+            <Route path="/pharmacist/pharmacy" component={Pharmacy} />
+            <Route exact path="/pharmacist"
+              render={props => <Redirect to={{pathname: '/pharmacist/dashboard', state: {from: props.location}}} />} />
+          </Switch>
+        </Col>
+      </Row>
+    </Container>
   )
 }
 
-const Dashboard = () => {
-  return <Container fluid className="bg-light vh-100">
-    <Row className="h-100">
-      <Col xs={2} md={1} lg={4} xl={3} className="d-flex flex-column">
-        <SideBar />
+const SideBar = () => {
+  let auth = useAuth()
+  return <Jumbotron id="sideBar"
+            className="bg-sky-blue text-light h-100 my-3 p-4" >
+    <Row className="h-100 flex-column">
+      <Col xs={"auto"} className="flex-grow-0">
+        <h1 className="fw-200">PharmaApp</h1>
       </Col>
-      <Col>
-        <DashboardContent />
+      <Col xs={5} className="my-3 d-flex flex-column justify-content-around">
+        <a href="/pharmacist/dashboard"><h2>Dashboard</h2></a>
+        <a href="/pharmacist/inventory"><h2>Inventory</h2></a>
+        <a href="/pharmacist/orders"><h2>Orders</h2></a>
+        <a href="/pharmacist/pharmacy"><h2>My Pharmacy</h2></a>
+      </Col>
+      <Col xs={"auto"} className="mt-auto d-flex flex-column">
+        <a href="/" className=""><h3>Back to Home</h3></a>
+        <hr />
+        <Button variant="dark-red" className="mt-2" onClick={e => {
+            e.preventDefault()
+            auth.signOut()
+          }}>Sign Out</Button>
       </Col>
     </Row>
+    
+  </Jumbotron>
+}
+
+const Inventory = () => {
+  return <Container fluid className="h-100 d-flex flex-column px-0">
+    <DashboardHeader title="Inventory Tracker"
+      subtitle="View, add and update medications" />
   </Container>
 }
 
-const SideBar = () => {
-  return <Jumbotron className="h-100 my-3 p-4" id="sideBar">
-    <h1 className="fw-200">PharmaApp</h1>
-  </Jumbotron>
+const Orders = () => {
+  return <Container fluid className="h-100 d-flex flex-column px-0">
+    <DashboardHeader title="Orders"
+      subtitle="View orders and history" />
+  </Container>
+}
+
+const Pharmacy = () => {
+  return <Container fluid className="h-100 d-flex flex-column px-0">
+    <DashboardHeader title="My Pharmacy"
+      subtitle="View or update your pharmacy's information" />
+  </Container>
+}
+
+const Dashboard = () => {
+  return <DashboardContent />
 }
 
 const DashboardContent = () => {
   return <Container fluid className="h-100 d-flex flex-column px-0">
-    <DashboardHeader />
+    <DashboardHeader
+      title="Dashboard" subtitle="Your pharmacy at a glance"/>
 
     <DashboardRow>
       <WidgetCol hasNeighbor>
@@ -62,21 +112,15 @@ const DashboardContent = () => {
   </Container>
 }
 
-const DashboardHeader = () => {
+const DashboardHeader = ({ title, subtitle }) => {
   let auth = useAuth()
-
   return <>
-    <Row className="my-2 justify-content-between align-items-center">
-      <Col xs={"auto"}><a href="/">{'<'} Back to Home</a></Col>
-      <Col xs={"auto"}><Button onClick={e => {
-          e.preventDefault()
-          auth.signOut()
-        }}>Sign Out</Button>
+    <Row className="align-items-baseline mt-3">
+      <Col xs={12} xl={"auto"}>
+        <h1 className="fw-800 mb-1 mb-xl-2">{title ? title : 'Provide title'}</h1>
       </Col>
-    </Row>
-    <Row>
-      <Col>
-        <h1 className="fw-800">Dashboard</h1>
+      <Col xs={12} xl={"auto"}>
+        <h2 className="fw-200">{subtitle ? subtitle : 'Provide subtitle'}</h2>
       </Col>
     </Row>
   </>
