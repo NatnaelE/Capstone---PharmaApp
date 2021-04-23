0714f24d7b69cca5435cf2e2c99a2274698dc9c2
Kyler Sakumoto <kylerws@uw.edu>
2021-04-22 21:54:17 -0700

    Added Back4App Parse connection

---
+++ client/src/constants/environments.js
@@ -0,0 +1,7 @@
+// Contains exports for Back4App API
+
+module.exports = {
+  APPLICATION_ID: 'eSM0pbSptlA7Voai3GdIiTF53DFAKa0rHo8jyU0c',
+  JAVASCRIPT_KEY: 'O7WkKwOzYin8PyZzFleQcfE2WviKjXRMPbjySCrs',
+  SERVER_URL: 'https://parseapi.back4app.com/'
+}
