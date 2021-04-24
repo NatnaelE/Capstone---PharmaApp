f645e44ece6fcf0aca007706bfa6fc40995b5230
Kyler Sakumoto <kylerws@uw.edu>
2021-04-23 20:41:21 -0700

    Added page for testing cloud functions

---
+++ client/src/dev/TestCloud.js
@@ -0,0 +1,33 @@
+import React from 'react'
+import { Container, Row, Col, Button } from 'react-bootstrap'
+import Parse from 'parse';
+
+//// Edit these fields ////
+const FUNCTION_NAME = 'getByBrandNameExact'
+const PARAMS = {
+  brandName: 'Advil'
+}
+
+
+// Runs the function
+function testCloudFunction() {
+  // Return if empty to avoid crashes
+  if (!FUNCTION_NAME) return
+
+  // Run function and console log results
+  Parse.Cloud.run(FUNCTION_NAME, PARAMS)
+    .then(console.log)
+    .catch(console.error)
+}
+
+// Component to display button
+const TestCloud = () => {
+  return <Container fluid className="vh-100 mt-5 pt-5">
+    <h5>Test Cloud Functions</h5>
+    <p className="fs-11 mb-1">Set the function name and params in src/dev/TestCloud.js</p>
+    <p className="fs-11">Check the console for results</p>
+    <Button onClick={testCloudFunction}>Run Function</Button>
+  </Container>
+}
+
+export default TestCloud
