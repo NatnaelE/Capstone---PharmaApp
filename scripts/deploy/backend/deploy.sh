7d98c819338479814bddf441b4409b1ec261b62f
Kyler Sakumoto <kylerws@uw.edu>
2021-05-04 12:59:24 -0700

    Fixed deploy scripts

--- scripts/deploy/backend/deploy.sh
+++ scripts/deploy/backend/deploy.sh
@@ -4,24 +4,35 @@
 export PROJECT_DIR=~/dev/Capstone---PharmaApp
 
 # Deploy directory
-export DEPLOY_DIR=$PROJECT_DIR/scripts/deploy/sequelize
+export DEPLOY_DIR=$PROJECT_DIR/scripts/deploy/backend
 
 echo -e "\n> Deploying Backend ..."
 
-cd $PROJECT_DIR
+# Change to server directory
+cd $PROJECT_DIR/server
 echo -e "> Working directory: " $(pwd) "\n"
 
-# Remove old
-# ssh -tt ec2-user@ec2-54-212-108-32.us-west-2.compute.amazonaws.com rm -r postgres
-
 # Delete node_modules
-rm -r $PROJECT_DIR/server/node_modules
-rm -r $PROJECT_DIR/server/src/node_modules
+# rm -r ./node_modules
 
 # Copy /server directory into EC2
+# sftp -r \
+#   ec2-user@ec2-54-212-108-32.us-west-2.compute.amazonaws.com: \
+#   <<< "put ./server"
+
+# Copy src
 sftp -r \
-  ec2-user@ec2-54-212-108-32.us-west-2.compute.amazonaws.com: \
-  <<< "put ./server"
+  ec2-user@ec2-54-212-108-32.us-west-2.compute.amazonaws.com:server \
+  <<< "put ./src"
+
+# Copy Docker and npm files
+sftp ec2-user@ec2-54-212-108-32.us-west-2.compute.amazonaws.com:server << EOF
+  put .dockerignore
+  put Dockerfile
+  put docker-compose.yml
+  put package-lock.json
+  put package.json
+EOF
 
 echo -e "\n> Server files copied!\n"
 
