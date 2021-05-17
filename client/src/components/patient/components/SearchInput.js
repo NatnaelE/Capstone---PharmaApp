64bd7c0ae0e9a128b5f6f22f998d495b1a53a8c2
Kyler Sakumoto <kylerws@uw.edu>
2021-05-17 14:43:22 -0700

    Refactored search inputs

--- client/src/components/patient/components/SearchInput.js
+++ client/src/components/patient/components/SearchInput.js
@@ -1,24 +1,23 @@
 import React, { useState, useEffect } from 'react'
 import { useSearch } from '../../../hooks/useSearch'
-// import { usePosition } from '../../../hooks/usePosition'
+import { useForm, useController, useWatch } from 'react-hook-form'
+import usePlacesAutocomplete from 'use-places-autocomplete'
 
-import { useForm, useController, useWatch, Controller } from 'react-hook-form'
-import { Row, Col, Form, Button, InputGroup } from 'react-bootstrap'
 import { Typeahead, AsyncTypeahead } from 'react-bootstrap-typeahead'
-import usePlacesAutocomplete from "use-places-autocomplete";
+import { Button, Col, Form, InputGroup } from 'react-bootstrap'
 
 // Icons
-import { LocalPharmacy, LocationOn, Search } from '@material-ui/icons';
+import { LocalPharmacy, LocationOn, Search } from '@material-ui/icons'
 
 // Autocomplete data
 import medData from '../../../constants/medData.json'
-// import { initialize } from 'parse'
 
 
 const SearchController = ({ id, labelKey, useLocation, cloudFunction }) => {
+  // Hook into search context provider
   const { loading, params } = useSearch()
-  // console.log(params)
-  // console.log(params ? params.med : 'no value found')
+
+  // Init React Hook Form
   const { handleSubmit, control, formState: { errors, isSubmitting }, clearErrors, setValue, getValues } = useForm({
     defaultValues: {
       medication: [],
@@ -26,24 +25,10 @@ const SearchController = ({ id, labelKey, useLocation, cloudFunction }) => {
     }
   });
 
-  // console.log(getValues())
-  // console.log(getValues())
-
+  // Control validation state
   const [validated, setValidated] = useState(false)
-  const [options, setOptions] = useState([])
 
-  // Load local med data
-  useEffect(() => {
-    // setValue("medication", [params.med])
-    const parsed = medData.map(d => {
-      return d[labelKey]
-    }).filter(d => {
-      return typeof d === "string"
-    })
-
-    setOptions([...new Set(parsed)])
-  }, [])
-
+  // Control field values on useSearch param change
   useEffect(() => {
     if (params) {
       setValue("medication", [params.med])
@@ -54,6 +39,12 @@ const SearchController = ({ id, labelKey, useLocation, cloudFunction }) => {
     }
   }, [params])
 
+  // Control validation state on error change
+  useEffect(() => {
+    errors ? setValidated(false) : setValidated(true)
+  }, [errors, setValidated])
+
+  // Handle form submit
   const onSubmit = async (data, e) => {
     e.preventDefault()
     console.log("SearchInput: submit fired")
@@ -67,110 +58,136 @@ const SearchController = ({ id, labelKey, useLocation, cloudFunction }) => {
     }
   }
 
-  useEffect(() => {
-    errors ? setValidated(false) : setValidated(true)
-  }, [errors, setValidated])
-
-  const props = {}
-  props.renderInput = ({
-    inputRef,
-    referenceElementRef,
-    ...inputProps
-  }) => (
-    <InputGroup size="lg" id="custom-search">
-      <InputGroup.Prepend>
-        <InputGroup.Text className="px-3 py-0">
-          <LocalPharmacy className="fs-1-1" />
-        </InputGroup.Text>
-      </InputGroup.Prepend>
-      <Form.Control
-        {...inputProps}
-        ref={(node) => {
-          inputRef(node);
-          referenceElementRef(node);
-        }}
-      />
-      
-    </InputGroup>
-  )
-
-  return (
-    
+  return (    
     <Form validated={validated} onSubmit={handleSubmit(onSubmit)}>
       <Form.Row className="align-items-center">
-      <Form.Group as={Col} xs={useLocation && 8} className="mb-0">
-      {/* <InputLabel icon={LocalPharmacy} label={useLocation ? 'Find Medications' : 'Search Medications'} /> */}
-      <div onClick={() => clearErrors("medication")}>
-        <Controller
-          name="medication"
-          // defaultValue={params ? [params.med] : []}
-          rules={{ required: true }}
-          control={control}
-          render={({
-            field: { ref, ...fields },
-            fieldState: { invalid, ...fieldState },
-            formState,
-          }) => (
-            <Typeahead
-              {...props}
-              {...fields}
-              {...fieldState}
-
-              id={(id ? id : 'X') + '-medication-search'}
-              labelKey={"label"}
-              options={options}
-              // defaultSelected={params ? [params.med] : []}
-              selected={getValues("medication")}
-
-              isInvalid={invalid}
-              disabled={isSubmitting || loading}
-              clearButton
-              allowNew
-              placeholder="What medication do you need?"
-              size="lg"
-            />
-          )}  
-        />
-        <Form.Control.Feedback type="invalid" tooltip 
-          className={`${errors.medication ? "d-block" : "d-none"} ml-3`}>
-          Please enter at least 1 medication
-        </Form.Control.Feedback>
-        </div>
-      </Form.Group>
-      
-      {useLocation && (
-        <Form.Group as={Col} className="mb-0">
-          {/* <InputLabel icon={LocationOn} label="Near" /> */}
-          <div onClick={() => clearErrors("location")}>
-            <LocationInput 
-              name="location" 
-              control={control} 
-              id={id} 
-              initial={params ? [{location: params.loc}] : []}
-              getValues={getValues}
-              searchLoading={loading} />
+        <Form.Group as={Col} xs={useLocation && 8} className="mb-0">
+          <div onClick={() => clearErrors("medication")}>
+            <MedicationInput 
+              name="medication"
+              control={control}
+              labelKey={labelKey}
+              id={id} />
 
             <Form.Control.Feedback type="invalid" tooltip 
-              className={`${errors.location ? "d-block" : "d-none"} ml-3`}>
-              Please enter a location
+              className={`${errors.medication ? "d-block" : "d-none"} ml-3`}>
+              Please enter at least 1 medication
             </Form.Control.Feedback>
           </div>
         </Form.Group>
-      )}
-      <Form.Group as={Col} xs={"auto"} className="mb-0 d-flex align-items-end">
-        <Button variant="green" size="lg" id="search-button"
-          className="p-3"
-          style={{borderRadius: '45px'}}
-          type="submit"
-          disabled={isSubmitting || loading}><Search /></Button>
-      </Form.Group>
+        
+        {useLocation && (
+          <Form.Group as={Col} className="mb-0">
+            {/* <InputLabel icon={LocationOn} label="Near" /> */}
+            <div onClick={() => clearErrors("location")}>
+              <LocationInput 
+                name="location" 
+                control={control} 
+                id={id} 
+                searchLoading={loading} />
+
+              <Form.Control.Feedback type="invalid" tooltip 
+                className={`${errors.location ? "d-block" : "d-none"} ml-3`}>
+                Please enter a location
+              </Form.Control.Feedback>
+            </div>
+          </Form.Group>
+        )}
+        
+        <Form.Group as={Col} xs={"auto"} className="mb-0 d-flex align-items-end">
+          <Button variant="green" size="lg" id="search-button"
+            className="p-3"
+            style={{borderRadius: '45px'}}
+            type="submit"
+            disabled={isSubmitting || loading}><Search /></Button>
+        </Form.Group>
       </Form.Row>
     </Form>
-    
   )
 }
 
-const LocationInput = ({ name, control, id, searchLoading, initial, getValues, ...rest }) => {
+
+const MedicationInput = ({ name, control, labelKey, id }) => {
+  const { loading } = useSearch()             // get useSearch loading state
+  const [options, setOptions] = useState([])  // control input options
+
+  // On mount, load local med data into options
+  useEffect(() => {
+    const parsed = medData.map(d => {
+      return d[labelKey]
+    }).filter(d => {
+      return typeof d === "string"
+    })
+
+    setOptions([...new Set(parsed)])
+  }, [])
+
+  // RHF controller props
+  const {
+    field: { ref, ...inputProps },
+    fieldState: { invalid, ...fieldState },
+    formState: { isSubmitting }
+  } = useController({
+    name,
+    control,
+    rules: { required: true }
+  });
+
+  // Watch RHF field value
+  const selected = useWatch({
+    control,
+    name
+  });
+
+  // React Typeahead render props
+  const renderProps = {
+    renderInput: ({
+      inputRef,
+      referenceElementRef,
+      ...inputProps
+    }) => (
+      <InputGroup size="lg" id="custom-search">
+        <InputGroup.Prepend>
+          <InputGroup.Text className="px-3 py-0">
+            <LocalPharmacy className="fs-1-1" />
+          </InputGroup.Text>
+        </InputGroup.Prepend>
+        <Form.Control
+          {...inputProps}
+          ref={(node) => {
+            inputRef(node);
+            referenceElementRef(node);
+          }}
+        />
+      </InputGroup>
+    )
+  }
+
+  return (
+    <Typeahead
+      {...inputProps}
+      {...fieldState}
+
+      {...renderProps}
+
+      id={(id ? id : 'X') + '-medication-search'}
+      labelKey={"label"}
+
+      options={options}
+      selected={selected}
+      isInvalid={invalid}
+      disabled={isSubmitting || loading}
+
+      clearButton
+      allowNew
+      placeholder="What medication do you need?"
+      size="lg"
+    />
+  )
+}
+
+
+const LocationInput = ({ name, control, id, searchLoading }) => {
   // RHF controller props
   const {
     field: { ref, onChange, ...inputProps },
@@ -181,6 +198,12 @@ const LocationInput = ({ name, control, id, searchLoading, initial, getValues, .
     control,
     rules: { required: true },
     defaultValue: [],
+  });
+
+  // Watch RHF field state
+  const selected = useWatch({
+    control,
+    name: 'location'
   });
 
   // usePlacesAutocomplete hook and props
@@ -197,15 +220,9 @@ const LocationInput = ({ name, control, id, searchLoading, initial, getValues, .
     // debounce: 300,
   });
 
+  // Control async loading and option state
   const [isLoading, setIsLoading] = useState(loading)
   const [options, setOptions] = useState([])
-  // const [selected, setSelected] = useState(initial)
-
-  const selected = useWatch({
-    control,
-    name: 'location',
-    // defaultValue: 'default' // default value before the render
-  });
 
   // Custom render styling props for Typeahead
   const renderProps = {
@@ -227,16 +244,23 @@ const LocationInput = ({ name, control, id, searchLoading, initial, getValues, .
             referenceElementRef(node);
           }}
         />
-        
       </InputGroup>
     )
   }
 
+  // Syncs UPA responses with Typeahead options
+  useEffect(() => {
+    setOptions(data.map(d => ({
+      location: d.description
+    })))
+    setIsLoading(false)
+  }, [data, setOptions])
+
   // Pass typed value from Typeahead to usePlacesAutocomplete
   const handleSearch = query => {
     console.log(query)
-    setIsLoading(true)
-    setValue(query)
+    setIsLoading(true)  // show loading state
+    setValue(query)     // set UPA query
   }
   
   // Handle Typeahead selection
@@ -249,37 +273,11 @@ const LocationInput = ({ name, control, id, searchLoading, initial, getValues, .
     }
   }
 
-  // useEffect(() => {
-  //   // console.log(initial)
-  //   // initial && setValue(initial)
-  //   console.log(selected)
-  //   console.log(initial)
-  // }, [])
-
-  // Syncs UPA responses with Typeahead options
-  useEffect(() => {
-    setOptions(data.map(d => ({
-      location: d.description
-    })))
-    setIsLoading(false)
-  }, [data, setOptions])
-
-  // // Log usePlacesAutocomplete status
-  // useEffect(() => {
-  //   console.log("usePlacesAutocomplete Reponse Status: " + status)
-  // }, [status])
-
-  // Log usePlacesAutocomplete value
-  useEffect(() => {
-    console.log(value)
-  }, [value])
-
   return (
     <AsyncTypeahead
       {...inputProps}
       {...fieldState}
       {...renderProps}
-      {...rest}
       
       filterBy={() => true}
       labelKey="location"
@@ -312,8 +310,4 @@ const LocationInput = ({ name, control, id, searchLoading, initial, getValues, .
 //     </Col>
 //   </Row>
 // }
-
-// const locationOptions = [
-//   'My Current Location'
-// ]
 export { SearchController }
